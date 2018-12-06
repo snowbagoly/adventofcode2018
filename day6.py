8714b003058817d7debff4e14863de04d970a8f7
snowbag <leona.thet@gmail.com>
2018-12-06 20:43:59 +0100

    day 6 cleaned up a bit

--- day6.py
+++ day6.py
@@ -7,38 +7,38 @@ def neighbours(x,y):
 	return [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
 def is_border_point(p,lx,rx,ly,ry):
 	return p[0] in (lx,rx) or p[1] in (ly,ry)
-def bfs(starters,lx,rx,ly,ry):
-	distances = { starter: (0,[starter]) for starter in starters}
-	borders = lx,ry,ly,ry
+def is_within_border(p,lx,rx,ly,ry):
+	return lx<=p[0] and p[0]<=rx and ly<=p[1] and p[1]<=ry
+
+def bfs(starters,borders):
+	distances = { starter: (0,{starter}) for starter in starters}
 	q = list(starters)
 	infinite_points = set(starter for starter in starters if is_border_point(starter,*borders))
 	while q:
 		p = q.pop(0)
-		dist_from = distances[p][1][0]
-		dist_p = distances[p][0]
+		dist_p,dist_from = distances[p]
 		for n in neighbours(*p):
-			if lx<=n[0] and n[0]<=rx and ly<=n[1] and n[1]<=ry:
+			if is_within_border(n,*borders):
 				was_reached = n in distances
-				must_modify = not was_reached or \
-				 (distances[n][0] >= dist_p + 1 and dist_from not in distances[n][1])
+				must_modify = not was_reached or distances[n][0] >= dist_p + 1
 				if was_reached and must_modify:
-					distances[n][1].append(dist_from)
-				elif not was_reached and must_modify:
-					distances[n] = (dist_p + 1,[dist_from])
+					distances[n][1].update(dist_from)
+				elif not was_reached:
+					distances[n] = (dist_p + 1,set(dist_from))
 					q.append(n)
-					if dist_from not in infinite_points and is_border_point(n,*borders):
-						infinite_points.add(dist_from)
+					if is_border_point(n,*borders):
+						infinite_points.update(dist_from)
 	return distances, infinite_points
 
-coords = list(map(lambda d: tuple(map(int,d.split(", "))), s.split("\n")))
-xcoords,ycoords = map(lambda c: c[0], coords), map(lambda c: c[1], coords)
+coords = map(lambda d: tuple(map(int,d.split(", "))), s.split("\n"))
+xcoords,ycoords = zip(*coords)
 borders = min(xcoords),max(xcoords),min(ycoords),max(ycoords)
-distances,infinite_points = bfs(coords,*borders)
 
+distances,infinite_points = bfs(coords,borders)
 counter = {coord : 0 for coord in coords}
 for p in distances:
 	if len(distances[p][1]) == 1:
-		add_to = distances[p][1][0]
+		add_to = next(iter(distances[p][1]))
 		if add_to not in infinite_points:
 			counter[add_to] += 1
 maximum = max(counter, key=counter.get)
